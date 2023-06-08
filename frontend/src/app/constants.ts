2ff22d2fb02075c8c91582530e8b68c1103ec657
Gaglia <francescogaglianese93@gmail.com>
2023-06-08 16:43:55 +0200

    Implementato service per le richieste REST

---
+++ frontend/src/app/constants.ts
@@ -0,0 +1,10 @@
+export const API={
+    baseUrl:"http://localhost:8080",
+    utente:"/utente",
+    product:"/prodotto",
+    registrati:"/registrati",
+    listaUtenti:"/listaUtenti"
+}
+export const APP_CONFIG={
+
+}
--- frontend/src/app/seller-auth/seller-auth.component.ts
+++ frontend/src/app/seller-auth/seller-auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, of } from 'rxjs';
+import { ApiService } from '../api.service';
+import { API } from '../constants';
 
 @Component({
   selector: 'app-seller-auth',
@@ -9,13 +11,15 @@ import { catchError, of } from 'rxjs';
 })
 export class SellerAuthComponent implements OnInit{
 
-  constructor(private http: HttpClient) {}
+  constructor(private apiService:ApiService) {}
 
   ngOnInit(): void {
       
   }
 
   signUp(data:object): void{
-    this.http.post('http://localhost:8080/utente/registrati',data).subscribe((d)=>{console.warn(d)});
+    this.apiService.makeRequest('post',API.utente+API.registrati,data).subscribe((d)=>{
+      console.warn(d);
+    })
   }
 }
